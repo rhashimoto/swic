82cad229304bb08de5da8dfeeba8fe1f0d240f35
Roy Hashimoto <roy@shoestringresearch.com>
2026-05-09 08:17:33 -0700

    Reset counts for modified sources.

--- src/persistence.js
+++ src/persistence.js
@@ -4,6 +4,9 @@ import { wrap } from "idb";
 
 const dbPromise = openIDB().then(db => wrap(db));
 
+const SOURCE_CACHE_NAME = 'swic-cache-sources';
+const cachePromise = caches.open(SOURCE_CACHE_NAME);
+
 /**
  * @returns {Promise<IDBDatabase>}
  */
@@ -26,7 +29,9 @@ export async function clearDB() {
   Array.from(db.objectStoreNames).forEach(storeName => {
     tx.objectStore(storeName).clear();
   });
-  await tx.done;
+
+  const cache = await cachePromise;
+  cache.keys().then(keys => Promise.all(keys.map(key => cache.delete(key))));
 }
 
 /**
@@ -47,16 +52,48 @@ export async function loadDataFromDB() {
  * @param {Iterable<[string, CoverageMaps]>} maps
  */
 export async function saveMapsToDB(maps) {
-  const db = await dbPromise;
-  const tx = db.transaction(['maps'], 'readwrite');
-  const store = tx.objectStore('maps');
+  const [db, cache] = await Promise.all([dbPromise, cachePromise]);
+
+  // Determine if any of the source files have changed by comparing ETags.
+  // If a file has changed, we'll need to reset its counts. This must be
+  // done outside the IndexedDB transaction because asynchronous operations
+  // for cache and fetch are required.
+  const contentChanged = new Set();
+  await Promise.all(Array.from(maps, async ([path]) => {
+    // Get the cache entry for this path.
+    const request = new Request(path);
+    const cachedResponse = await cache.match(request);
+
+    // Fetch the current ETag for the path.
+    const response = await fetch(request, { method: 'HEAD' });
+    if (response.ok) {
+      const eTag = response.headers.get('ETag');
+      if (eTag && eTag !== cachedResponse?.headers.get('ETag')) {
+        // ETag changed, so mark this path's counts for reset.
+        contentChanged.add(path);
+        cache.put(request, response);
+      }
+    }
+  }));
+
+  // Save all the maps and reset counts as needed.
+  const tx = db.transaction(['maps', 'counts'], 'readwrite');
+  const mapsStore = tx.objectStore('maps');
+  const countsStore = tx.objectStore('counts');
   for (const [path, mapping] of maps) {
-    store.put({
+    mapsStore.put({
       path,
       statementMap: mapping.statementMap,
       fnMap: mapping.fnMap,
       branchMap: mapping.branchMap
     });
+
+    if (contentChanged.has(path)) {
+      // Reset counts for modified file.
+      console.debug(`Reset counts for ${path}`);
+      countsStore.delete(path);
+    }
   }
   await tx.done;
+
 }
